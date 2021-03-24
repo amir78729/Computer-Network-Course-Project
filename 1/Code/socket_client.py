9748a4ecb01d4f7bcf696acf7cc62e96f3196282
amir78729 <amir78729@gmail.com>
2021-03-25 00:15:44 +0430

    q1 is done

--- 1/Code/socket_client.py
+++ 1/Code/socket_client.py
@@ -1,11 +1,21 @@
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 53        # The port used by the server
+msgFromClient = "Hello Server:)"
+bytesToSend = str.encode(msgFromClient)
+server_IP = '80.80.80.80'
+server_IP = '127.0.0.1'  # uncomment for testing
+server_port = 53  # the port for DNS
+bufferSize = 512
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+# Creating a UDP socket at client side
+UDP_client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+print('UDP socket has been created!')
 
-print('Received', repr(data))
+# Send to server using created UDP socket
+UDP_client_socket.sendto(bytesToSend, (server_IP, server_port))
+print('The message has been sent to Server!\nPORT: {}\nADDRESS: {}'.format(server_port, server_IP))
+
+msgFromServer = UDP_client_socket.recvfrom(bufferSize)
+
+msg = "Message from Server {}".format(msgFromServer[0])
+print(msg)
