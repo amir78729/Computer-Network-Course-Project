9d14c85a2a609407b613882f3881bccbb7bed661
amir78729 <amir78729@gmail.com>
2021-03-25 21:25:10 +0430

    handling herror an indexError

--- 1/Code/Q3_DNS_queries.py
+++ 1/Code/Q3_DNS_queries.py
@@ -2,6 +2,16 @@ import socket
 import csv
 import pandas
 
+
+def write_on_csv(file_name):
+    with open(file_name, mode='w', newline="") as output_file:
+        csv_writer = csv.writer(output_file, delimiter=',')
+        csv_writer.writerow(['hostname', 'IP Address'])
+        for d in data:
+            if any(d):
+                csv_writer.writerow(d)
+
+
 if __name__ == '__main__':
     while True:
         print('-----------------------------')
@@ -25,26 +35,28 @@ if __name__ == '__main__':
                     csv_reader = csv.reader(csv_file, delimiter=',')
                     line_count = 0
                     for row in csv_reader:
-                        row_list = []
-                        if line_count == 0:
-                            # print(f'Column names are {", ".join(row)}')
-                            line_count += 1
-                        else:
-                            hostname = row[0]
-                            A_record = socket.gethostbyname(hostname)
+                        try:
+                            row_list = []
+                            if line_count == 0:
+                                # print(f'Column names are {", ".join(row)}')
+                                line_count += 1
+                            else:
+                                hostname = row[0]
+                                row_list.append(hostname)
+                                A_record = socket.gethostbyaddr(hostname)
+                                print(A_record)
 
-                            row_list.append(hostname)
-                            row_list.append(A_record)
-                            line_count += 1
+                                row_list.append(A_record[2][0])
+                                line_count += 1
+                                data.append(row_list)
+                        except IndexError:
+                            pass
+                        except socket.herror:
+                            # print('Host not found')
+                            row_list.append(socket.gethostbyname(hostname))
                             data.append(row_list)
-
                 # writing to Q3_csv_output.csv
-                with open('Q3_csv_output.csv', mode='w', newline="") as output_file:
-                    csv_writer = csv.writer(output_file, delimiter=',')
-                    csv_writer.writerow(['hostname', 'A record'])
-                    for d in data:
-                        if any(d):
-                            csv_writer.writerow(d)
+                write_on_csv('Q3_csv_output.csv')
 
                 # printing the output
                 print(pandas.read_csv('Q3_csv_output.csv'))
@@ -53,6 +65,6 @@ if __name__ == '__main__':
             elif user_input == -1:
                 break
             else:
-                print('Wrong input! Try again.')
+                print('Something went wrong! Try again.')
         except (socket.gaierror, ValueError):
-            print('Wrong input! Try again.')
+            print('Something went wrong! Try again.')
