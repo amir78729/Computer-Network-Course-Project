3201db320e0be214543f7b76bd28a0db469d878f
amir78729 <amir78729@gmail.com>
2021-04-23 02:49:42 +0430

    sending messages

---
+++ 2/Client.py
@@ -0,0 +1,26 @@
+# Import socket module
+import socket
+import getpass
+
+
+
+if __name__ == '__main__':
+    # Create a socket object
+    s = socket.socket()
+    # Define the port on which you want to connect
+    port = 12345
+    # connect to the server on local computer
+    s.connect(('127.0.0.1', port))
+
+    option = int(input(' 1 - login\n 2 - create new account\n-1 - cancel'.upper()))
+    if option == 1:
+        print('login:')
+        username = input(' > username:  '.upper())
+        password = input(' > password:  '.upper())
+        msg = 'login {} {}'.format(username, password)
+        s.send(msg.encode('utf-8'))
+
+    # receive data from the server
+    print(s.recv(1024))
+    # close the connection
+    s.close()
--- 2/Server.py
+++ 2/Server.py
@@ -1,36 +1,55 @@
 # first of all import the socket library 
 import socket
 
-# next create a socket object 
-s = socket.socket()
-print("Socket successfully created")
+class Server:
+    def __init__(self):
+        self.users = []
 
-# reserve a port on your computer in our 
-# case it is 12345 but it can be anything 
-port = 12345
+    def run_server(self):
+        # next create a socket object
+        s = socket.socket()
+        print("Socket successfully created")
 
-# Next bind to the port 
-# we have not typed any ip in the ip field 
-# instead we have inputted an empty string 
-# this makes the server listen to requests 
-# coming from other computers on the network 
-s.bind(('', port))
-print("socket binded to %s" % (port))
+        # reserve a port on your computer in our
+        # case it is 12345 but it can be anything
+        port = 12345
 
-# put the socket into listening mode 
-s.listen(5)
-print("socket is listening")
+        # Next bind to the port
+        # we have not typed any ip in the ip field
+        # instead we have inputted an empty string
+        # this makes the server listen to requests
+        # coming from other computers on the network
+        s.bind(('', port))
+        print("socket binded to %s" % (port))
 
-# a forever loop until we interrupt it or 
-# an error occurs 
-while True:
+        # put the socket into listening mode
+        s.listen(5)
+        print("socket is listening")
 
-    # Establish connection with client.
-    c, addr = s.accept()
-    print('Got connection from', addr)
+        # a forever loop until we interrupt it or
+        # an error occurs
+        while True:
 
-    # send a thank you message to the client.
-    c.send(b'Thank you for connecting')
+            # Establish connection with client.
+            print(self.users)
+            c, addr = s.accept()
+            print('Got connection from', addr)
 
-    # Close the connection with the client
-# c.close()
+            # send a thank you message to the client.
+            # c.send(b'Thank you for connecting')
+            recieved_message = c.recv(2048).decode('utf-8').split()
+            if recieved_message[0] == 'login':
+                if recieved_message[1] in self.users :
+                    c.send('user is in the list'.upper().encode('utf-8'))
+                else:
+                    c.send('user does not exist'.upper().encode('utf-8'))
+
+
+
+            # Close the connection with the client
+        # c.close()
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.run_server()
