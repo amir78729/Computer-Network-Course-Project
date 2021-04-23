841c543a2d7b6401952184e3165def884d5c027a
amir78729 <amir78729@gmail.com>
2021-04-23 20:44:45 +0430

    db is working

--- 2/Server.py
+++ 2/Server.py
@@ -2,22 +2,40 @@
 import socket
 import datetime
 import csv
+from database_functions import *
 
 
 class Server:
     def __init__(self):
         # retrieve users to a dictionary from a csv file
-        with open('users.csv', mode='r') as infile:
-            reader = csv.reader(infile)
-            self.users = {rows[0]: rows[1] for rows in reader}
+        # with open('users.csv', mode='r') as infile:
+        #     reader = csv.reader(infile)
+        #     self.users = {rows[0]: rows[1] for rows in reader}
+
+        git_database = "users.db"
+        self.conn = create_connection(git_database)
+
+        create_user_table_query = """ CREATE TABLE IF NOT EXISTS users_info (
+                                                    username text PRIMARY KEY,
+                                                    password text
+                                                ); """
+
+        if self.conn is not None:
+            # create projects table
+            create_table(self.conn, create_user_table_query)
+
+        else:
+            print("Error... Database is NOT active!.")
 
     def add_user(self, username, password):
         # add user to dictionary
-        self.users.update({username: password})
+        # self.users.update({username: password})
         # add user to 'users.csv'
-        with open('users.csv', 'a+') as write_obj:
-            csv_writer = csv.writer(write_obj)
-            csv_writer.writerow([username, password])
+        # with open('users.csv', 'a+') as write_obj:
+        #     csv_writer = csv.writer(write_obj)
+        #     csv_writer.writerow([username, password])
+
+        insert_into_table(self.conn, 'users_info', 'username, password', (username, password))
 
     def send_message_to_client(self, c, message):
         c.send(message.upper().encode('utf-8'))
@@ -25,10 +43,6 @@ class Server:
     def run_server(self):
         # next create a socket object
         s = socket.socket()
-        # print("Socket successfully created")
-
-        # reserve a port on your computer in our
-        # case it is 12345 but it can be anything
         port = 12345
 
         # Next bind to the port
@@ -37,20 +51,17 @@ class Server:
         # this makes the server listen to requests
         # coming from other computers on the network
         s.bind(('', port))
-        # print("socket binded to %s" % (port))
-
-        # put the socket into listening mode
         s.listen(15)
-        # print("socket is listening")
 
-        # a forever loop until we interrupt it or
-        # an error occurs
         while True:
+            print(get_table(self.conn, 'users_info'))
+
+
             print('- '*20)
-            print(self.users)
+            # print(self.users)
 
             # Establish connection with client.
-            print('{} users are connected to the server'.format(len(self.users)).upper())
+            # print('{} users are connected to the server'.format(len(self.users)).upper())
 
             print('waiting for clients...'.upper(), end='')
             c, addr = s.accept()
@@ -66,29 +77,38 @@ class Server:
                       '\n\tat      :\t{}'.format(received_message[0], addr[0], addr[1], datetime.datetime.now().strftime("%c")).upper())
 
                 if received_message[0] == 'login':
-                    if received_message[1] in self.users.keys():
-                        if self.users[received_message[1]] == received_message[2]:
-                            c.send('logged in successfully'.upper().encode('utf-8'))
+                    # if received_message[1] in self.users.keys():
+                    if check_if_user_exists(self.conn, received_message[1]):
+                        # if self.users[received_message[1]] == received_message[2]:
+                        if check_password(self.conn, received_message[1], received_message[2]):
+                            # c.send('logged in successfully'.upper().encode('utf-8'))
+                            self.send_message_to_client(c, 'logged in successfully')
                             print('\tstatus  :\tsuccessful'.upper())
                         else:
-                            c.send('wrong password!'.upper().encode('utf-8'))
+                            # c.send('wrong password!'.upper().encode('utf-8'))
+                            self.send_message_to_client(c, 'wrong password!')
                             print('\tstatus  :\twrong password'.upper())
                     else:
-                        c.send('user does not exist'.upper().encode('utf-8'))
+                        # c.send('user does not exist'.upper().encode('utf-8'))
+                        self.send_message_to_client(c, 'user does not exist')
                         print('\tstatus  :\tuser not found'.upper())
 
                 if received_message[0] == 'signup':
-                    if received_message[1] in self.users.keys():
-                        c.send('user already exists!'.upper().encode('utf-8'))
+                    # if received_message[1] in self.users.keys():
+                    if check_if_user_exists(self.conn, received_message[1]):
+                        # c.send('user already exists!'.upper().encode('utf-8'))
+                        self.send_message_to_client(c, 'user already exists!')
                         print('\tstatus  :\tuser already exist!'.upper())
                     else:
                         self.add_user(received_message[1], received_message[2])
-                        c.send('user created successfully'.upper().encode('utf-8'))
+                        # c.send('user created successfully'.upper().encode('utf-8'))
+                        self.send_message_to_client(c, 'user created successfully')
                         print('\tstatus  :\tsuccessful'.upper())
             except Exception as e:
                 print('oops...something went wrong:'.upper())
                 print(e)
-                c.send('oops, something went wrong!'.upper().encode('utf-8'))
+                # c.send('oops, something went wrong!'.upper().encode('utf-8'))
+                self.send_message_to_client(c, 'oops, something went wrong!')
             # Close the connection with the client
         # c.close()
 
---
+++ 2/database_functions.py
@@ -0,0 +1,114 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+    return conn
+
+
+def create_table(conn, query):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param query: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(query)
+    except Error as e:
+        print(e)
+
+
+def insert_into_table(conn, table, fields, values):
+    """
+    Create a new task
+    :param conn:
+    :param table: table name
+    :param values: records
+    :param fields: columns
+    :return:
+    """
+
+    sql = ''' INSERT INTO {}({})
+              VALUES(?,?) '''.format(table, fields)
+    cur = conn.cursor()
+    try:
+        cur.execute(sql, values)
+        conn.commit()
+    except Exception as e:
+        print(e)
+    return cur.lastrowid
+
+
+def get_table(conn, table):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM {}".format(table))
+    return cur.fetchall()
+
+
+def check_password(conn, username, password):
+    """
+
+    :param conn:
+    :param username:
+    :param password:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users_info WHERE username=\'{}\'".format(username))
+    result = cur.fetchall()
+    # print(result[0][1] == password)
+    return result[0][1] == password
+
+def check_if_user_exists(conn, username):
+    """
+    check if data is in the table
+    :param conn: the Connection object
+    :param username:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users_info WHERE username=?", (username,))
+    rows = cur.fetchall()
+    return not len(rows) == 0
+
+
+def get_response_from_cache(conn, hostname_record_recursion):
+    """
+    use the cache
+    :param conn: the Connection object
+    :param hostname_record_recursion:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT RESPONSE FROM dns_cache WHERE HOSTNAME_RECORD_RECURSION=?", (hostname_record_recursion,))
+    rows = cur.fetchall()
+    return rows
+
+
+def clear_cache(conn):
+    """
+    Delete all rows in the table
+    :param conn: Connection to the SQLite database
+    :return:
+    """
+    sql = 'DELETE FROM dns_cache'
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
